0bc022d3b22f0598407f49a951cdd0ffaa451538
Bartosz <43646817+Rippp@users.noreply.github.com>
2021-01-06 20:22:11 +0100

    Add files via upload
    Added day 1 solutions

---
+++ Day 1a/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_Day1a
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            string[] tab = File.ReadAllLines(filename);
+
+            for (int i = 1; i < tab.Length; i++)
+                for (int j = i + 1; j < tab.Length; j++)
+                    if (int.Parse(tab[i]) + int.Parse(tab[j]) == 2020)
+                        Console.WriteLine(int.Parse(tab[i]) * int.Parse(tab[j]));
+
+        }
+    }
+}

17843de6e5ea11f25fdbf324f9405d367dceedb3
Bartosz <43646817+Rippp@users.noreply.github.com>
2021-01-08 20:10:38 +0100

    Add files via upload
    Added day 12 solutions

---
+++ Day 12a/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_Day12a
+{
+    class Program
+    {
+        public enum dir: int
+        {
+            N = 0,
+            E = 1,
+            S = 2,
+            W = 3
+        }
+
+        public static void process_lines(string[] lines)
+        {
+            int[] dist = new int[4];
+            dir F = dir.E;
+            int arg;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                arg = int.Parse(lines[i][1..]);
+                switch (lines[i][0])
+                {
+                    case 'N':
+                        dist[(int)dir.N] += arg;
+                        break;
+                    case 'S':
+                        dist[(int)dir.S] += arg;
+                        break;
+                    case 'E':
+                        dist[(int)dir.E] += arg;
+                        break;
+                    case 'W':
+                        dist[(int)dir.W] += arg;
+                        break;
+                    case 'F':
+                        dist[(int)F] += arg;
+                        break;
+                    case 'L':
+                        for (int j = 0; j < arg; j += 90)
+                        {
+                            F--;
+                            if (F < 0) F = dir.W;
+                        }
+                        break;
+                    case 'R':
+                        for (int j = 0; j < arg; j += 90)
+                        {
+                            F++;
+                            if (F > dir.W) F = dir.N;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(Math.Abs(dist[(int)dir.N] - dist[(int)dir.S]) +
+                              Math.Abs(dist[(int)dir.E] - dist[(int)dir.W]));
+        }
+
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            string[] lines = File.ReadAllLines(filename);
+            process_lines(lines);
+        }
+    }
+}
---
+++ Day 12b/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_Day12b
+{
+    class Program
+    {
+        static void swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c; 
+        }
+
+        public static void process_lines(string[] lines)
+        {
+            int x = 0, y = 0;
+            int way_x = 10, way_y = 1;
+            int arg;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                arg = int.Parse(lines[i][1..]);
+                switch (lines[i][0])
+                {
+                    case 'N':
+                        way_y += arg;
+                        break;
+                    case 'S':
+                        way_y -= arg;
+                        break;
+                    case 'E':
+                        way_x += arg;
+                        break;
+                    case 'W':
+                        way_x -= arg;
+                        break;
+                    case 'F':
+                        x += way_x * arg;
+                        y += way_y * arg;
+                        break;
+                    case 'L':
+                        for(int j = 0; j < arg; j += 90)
+                        {
+                            swap<int>(ref way_x, ref way_y);
+                            way_x = -way_x;
+                        }
+                        break;
+                    case 'R':
+                        for (int j = 0; j < arg; j += 90)
+                        {
+                            swap<int>(ref way_x, ref way_y);
+                            way_y = -way_y;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(Math.Abs(x) + Math.Abs(y));
+        }
+
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            string[] lines = File.ReadAllLines(filename);
+            process_lines(lines);
+        }
+    }
+}
